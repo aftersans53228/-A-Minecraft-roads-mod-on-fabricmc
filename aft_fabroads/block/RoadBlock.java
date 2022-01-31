783455b1fcc28fe47060345ae0b90b8dea239e81
XieXiLin <55585872+XieXiLin158@users.noreply.github.com>
2022-01-31 14:10:14 +0800

    Delete aft_fabroads directory

--- aft_fabroads/block/RoadBlock.java
+++
@@ -1,9 +0,0 @@
-package net.aftersans53228.aft_fabroads.block;
-
-import net.minecraft.block.Block;
-
-public class RoadBlock extends Block {
-    public RoadBlock(Settings settings) {
-        super(settings);
-    }
-}
